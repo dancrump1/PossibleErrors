67414afc251b2a3f2786337e276599347a411080
Chris Gauthier <chrsgauthier5@gmail.com>
2019-01-13 15:21:41 -0500

    added delete button, logging out of submitted surveys (we can delete later if not good for this page), and hooked up some of the API / html routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,22 +3,25 @@ var db = require("../models");
 module.exports = function(app) {
   // Get all examples
   app.get("/api/examples", function(req, res) {
-    db.Pets.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+    db.Survey.findAll({}).then(function(surveyData) {
+      res.json(surveyData);
     });
   });
 
   // Create a new example
   app.post("/api/examples", function(req, res) {
-    db.Survey.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
+    console.log(req.body)
+    db.Survey.create(req.body).then(function(survey) {
+      res.json(survey);
     });
   });
 
   // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
+  app.delete("/api/examples", function(req, res) {
+    console.log(req.body);
+    db.Survey.destroy({ where: { id: req.body.id } }).then(function(survey) {
+      console.log(survey);
+      res.json(survey);
     });
   });
 };
